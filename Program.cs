55bf490a69625c2712dd37a28a7ab8a4fa3d9a98
hannmx <mxkud@mail.ru>
2022-11-13 13:39:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGB_HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int a = int.Parse(Console.ReadLine());
+            
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"Число {a} является четным");
+            }
+            else
+            {
+                Console.WriteLine($"Число {a} является нечетным");
+            }
+        }
+    }
+}
